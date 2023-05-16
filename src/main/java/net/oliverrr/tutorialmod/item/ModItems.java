bc48a69ac284b2d86a68ff2217b75958fb70523f
oliverrr123 <oliver.cingl@gmail.com>
2023-05-16 23:00:16 +0200

    added added chocolate & other chocolate stuff

--- src/main/java/net/oliverrr/tutorialmod/item/ModItems.java
+++ src/main/java/net/oliverrr/tutorialmod/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.oliverrr.tutorialmod.TutorialMod;
+import net.oliverrr.tutorialmod.item.custom.Chocolate;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -17,6 +18,16 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> CHOCOLATE = ITEMS.register("chocolate",
+            () -> new Chocolate(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+    public static final RegistryObject<Item> DROP_OF_CHOCOLATE = ITEMS.register("drop_of_chocolate",
+            () -> new Chocolate(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
+//    public static class Foods() {
+//        public static final Food CHOCOLATE;
+//    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/net/oliverrr/tutorialmod/item/custom/Chocolate.java
@@ -0,0 +1,23 @@
+package net.oliverrr.tutorialmod.item.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class Chocolate extends Item {
+    public Chocolate(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if (!level.isClientSide() && hand==InteractionHand.MAIN_HAND) {
+            player.sendSystemMessage(Component.literal("chocolat"));
+        }
+        return super.use(level, player, hand);
+    }
+}
